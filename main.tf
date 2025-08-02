8a1c66c2b0f6d9eb9c3092fa50323cfe74f4b3f8
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-08-02 08:41:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,178 +1,112 @@
+# main.tf
+
 provider "aws" {
   region = var.region
 }
 
-# Fetch the default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Fetch subnets associated with the default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# IAM Role for ECS task execution
-resource "aws_iam_role" "ecs_task_execution" {
+# Use pre-existing ECS task execution role
+data "aws_iam_role" "ecs_task_execution" {
   name = "ecs-task-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# ECS Cluster
 resource "aws_ecs_cluster" "strapi" {
-  name = "strapi-cluster"
+  name = "${var.app_name}-cluster"
 }
 
-# Security Group for ALB
-resource "aws_security_group" "lb" {
-  name        = "strapi-alb-sg"
-  description = "Allow HTTP"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+# Use pre-existing security group
+data "aws_security_group" "lb" {
+  filter {
+    name   = "group-name"
+    values = ["strapi-alb-sg"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id = data.aws_vpc.default.id
 }
 
-# Application Load Balancer
+# ALB
 resource "aws_lb" "this" {
-  name               = "strapi-alb"
+  name               = "${var.app_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb.id]
+  security_groups    = [data.aws_security_group.lb.id]
   subnets            = data.aws_subnets.default.ids
 }
 
-# Target Group
-resource "aws_lb_target_group" "strapi" {
-  name     = "strapi-tg"
-  port     = 1337
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-  target_type = "ip"
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-399"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
+# Use pre-existing Target Group
+data "aws_lb_target_group" "strapi" {
+  name = "strapi-tg"
 }
 
-# Listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.strapi.arn
+    target_group_arn = data.aws_lb_target_group.strapi.arn
   }
 }
 
-# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi-task"
+  family                   = "${var.app_name}-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  network_mode             = "awsvpc"
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution.arn
   container_definitions = jsonencode([
     {
-      name      = "strapi"
+      name      = "${var.app_name}"
       image     = var.image_url
-      essential = true
       portMappings = [
         {
-          containerPort = 1337
-          hostPort      = 1337
-          protocol      = "tcp"
+          containerPort = var.container_port
+          protocol       = "tcp"
         }
       ]
+      essential = true
     }
   ])
 }
 
-# ECS Service
 resource "aws_ecs_service" "strapi" {
-  name            = "strapi-service"
+  name            = "${var.app_name}-service"
   cluster         = aws_ecs_cluster.strapi.id
   task_definition = aws_ecs_task_definition.strapi.arn
+  desired_count   = var.desired_count
   launch_type     = "FARGATE"
 
   network_configuration {
     subnets         = data.aws_subnets.default.ids
-    security_groups = [aws_security_group.lb.id]
+    security_groups = [data.aws_security_group.lb.id]
     assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.strapi.arn
-    container_name   = "strapi"
-    container_port   = 1337
   }
 
   deployment_controller {
     type = "CODE_DEPLOY"
   }
 
+  load_balancer {
+    target_group_arn = data.aws_lb_target_group.strapi.arn
+    container_name   = var.app_name
+    container_port   = var.container_port
+  }
+
   depends_on = [aws_lb_listener.http]
 }
 
-# CodeDeploy App
-resource "aws_codedeploy_app" "ecs" {
+# Use existing CodeDeploy application
+data "aws_codedeploy_app" "ecs" {
   name = "strapi-codedeploy-app"
-  compute_platform = "ECS"
 }
 
-# CodeDeploy Deployment Group
 resource "aws_codedeploy_deployment_group" "ecs" {
-  app_name              = aws_codedeploy_app.ecs.name
-  deployment_group_name = "strapi-deploy-group"
-  service_role_arn      = aws_iam_role.ecs_task_execution.arn
-
-  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
+  app_name               = data.aws_codedeploy_app.ecs.name
+  deployment_group_name = "${var.app_name}-deploy-group"
+  service_role_arn       = data.aws_iam_role.ecs_task_execution.arn
 
   deployment_style {
-    deployment_type   = "BLUE_GREEN"
     deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
   }
 
   blue_green_deployment_config {
@@ -190,5 +124,22 @@ resource "aws_codedeploy_deployment_group" "ecs" {
   ecs_service {
     cluster_name = aws_ecs_cluster.strapi.name
     service_name = aws_ecs_service.strapi.name
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+}
+
+# Data Sources for default VPC and Subnets
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
