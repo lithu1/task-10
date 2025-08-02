8adc9b642e1ed059d6a8b9625ebcb326c4fe9c0b
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-08-02 07:59:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,11 @@ variable "app_name" {
 variable "container_port" {
   default = 1337
 }
+variable "image_url" {
+  description = "Docker image URL for the ECS task definition"
+  type        = string
+}
+variable "image_url" {
+  description = "Docker image URL for the ECS task definition"
+  type        = string
+}
