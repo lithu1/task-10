301ffc31fa1de22bf1e0d8e8f8c0bfdb85c7e632
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-08-02 16:38:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,25 @@
-variable "aws_region" {
-  default = "us-east-2"
+variable "image_uri" {
+  description = "Docker image URI"
+  type        = string
+}
+
+variable "task_execution_role_arn" {
+  description = "IAM role ARN for ECS task execution"
+  type        = string
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs"
 }
 
 variable "vpc_id" {
-  description = "VPC ID for networking"
   type        = string
-}
-
-variable "public_subnets" {
-  description = "List of public subnet IDs"
-  type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "List of private subnet IDs"
-  type        = list(string)
-}
-
-variable "image_uri" {
-  description = "Container image URI"
-  type        = string
-}
-
-variable "ecs_task_execution_role_arn" {
-  description = "ARN of ECS task execution IAM role"
-  type        = string
+  description = "VPC ID"
 }
 
 variable "codedeploy_role_arn" {
   description = "IAM role ARN for CodeDeploy"
   type        = string
 }
+
