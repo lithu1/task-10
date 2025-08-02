0edbd2fc13f8609fd3d29a969ca075365827053d
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-08-02 16:46:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,45 +2,49 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# ECS Cluster
+# Automatically use default VPC and its public subnets
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_ecs_cluster" "strapi" {
   name = "strapi-cluster"
 }
 
-# IAM Role for CodeDeploy (can be passed as a variable)
-# You can alternatively create this IAM role via Terraform (see bottom)
-# and use: codedeploy_role_arn = aws_iam_role.codedeploy.arn
-# This variable must be passed using TF_VAR_codedeploy_role_arn or tfvars
-# e.g., arn:aws:iam::607700977843:role/CodeDeployRole
-
-# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
-  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
+  execution_role_arn       = "arn:aws:iam::607700977843:role/ecsTaskExecutionRole"
 
   container_definitions = jsonencode([
     {
-      name  = "strapi"
-      image = var.image_uri
-      portMappings = [{
-        containerPort = 1337
-        hostPort      = 1337
-      }]
+      name      = "strapi"
+      image     = var.image_uri
       essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+          protocol      = "tcp"
+        }
+      ]
     }
   ])
-
-  execution_role_arn = var.task_execution_role_arn
 }
 
-# Security Group for ALB
 resource "aws_security_group" "alb" {
   name        = "alb-sg"
-  description = "Allow HTTP access"
-  vpc_id      = var.vpc_id
+  description = "Allow HTTP"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -57,59 +61,56 @@ resource "aws_security_group" "alb" {
   }
 }
 
-# Load Balancer
 resource "aws_lb" "strapi_alb" {
   name               = "strapi-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
-  subnets            = var.subnet_ids
+  subnets            = data.aws_subnets.public.ids
 }
 
-# Target Groups
 resource "aws_lb_target_group" "blue" {
   name     = "strapi-blue"
   port     = 1337
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id   = data.aws_vpc.default.id
   target_type = "ip"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
-resource "aws_lb_target_group" "green" {
-  name     = "strapi-green"
-  port     = 1337
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "ip"
-}
-
-# Listener
-resource "aws_lb_listener" "listener" {
+resource "aws_lb_listener" "strapi_listener" {
   load_balancer_arn = aws_lb.strapi_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.blue.arn
   }
 }
 
-# ECS Service with CodeDeploy
 resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi.id
   launch_type     = "FARGATE"
-  platform_version = "LATEST"
   desired_count   = 1
+
   deployment_controller {
     type = "CODE_DEPLOY"
   }
 
   network_configuration {
-    subnets         = var.subnet_ids
+    subnets          = data.aws_subnets.public.ids
+    security_groups  = [aws_security_group.alb.id]
     assign_public_ip = true
-    security_groups = [aws_security_group.alb.id]
   }
 
   load_balancer {
@@ -121,17 +122,15 @@ resource "aws_ecs_service" "strapi" {
   task_definition = aws_ecs_task_definition.strapi.arn
 }
 
-# CodeDeploy Application
-resource "aws_codedeploy_app" "ecs" {
-  name = "strapi-codedeploy-app"
+resource "aws_codedeploy_app" "strapi" {
+  name = "strapi-codedeploy"
   compute_platform = "ECS"
 }
 
-# CodeDeploy Deployment Group
-resource "aws_codedeploy_deployment_group" "ecs" {
-  app_name              = aws_codedeploy_app.ecs.name
-  deployment_group_name = "strapi-deploy-group"
-  service_role_arn      = var.codedeploy_role_arn
+resource "aws_codedeploy_deployment_group" "strapi" {
+  app_name               = aws_codedeploy_app.strapi.name
+  deployment_group_name  = "strapi-deploy-group"
+  service_role_arn       = var.codedeploy_role_arn
 
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
 
@@ -145,11 +144,9 @@ resource "aws_codedeploy_deployment_group" "ecs" {
       target_group {
         name = aws_lb_target_group.blue.name
       }
-      target_group {
-        name = aws_lb_target_group.green.name
-      }
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.listener.arn]
+
+      production_traffic_route {
+        listener_arn = aws_lb_listener.strapi_listener.arn
       }
     }
   }
