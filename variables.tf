ed0b65bc3cdd82c62c83140f53210fbac4dc5736
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-08-02 16:46:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,9 @@
 variable "image_uri" {
-  description = "Docker image URI"
+  description = "ECR image URI for the Strapi container"
   type        = string
-}
-
-variable "task_execution_role_arn" {
-  description = "IAM role ARN for ECS task execution"
-  type        = string
-}
-
-variable "subnet_ids" {
-  type        = list(string)
-  description = "List of subnet IDs"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID"
 }
 
 variable "codedeploy_role_arn" {
-  description = "IAM role ARN for CodeDeploy"
+  description = "IAM role ARN for CodeDeploy to access ECS"
   type        = string
 }
-
