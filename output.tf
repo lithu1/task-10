abd49ed959e2130f875327eb572a820ab60c528c
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-08-02 16:23:33 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
 output "alb_dns_name" {
-  value = aws_lb.this.dns_name
+  value = aws_lb.strapi_alb.dns_name
+  description = "Public DNS name of the Application Load Balancer"
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.strapi.name
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.strapi.name
 }
